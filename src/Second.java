29d88a7984fa8f3b675d2ab3171fc37d6cca62bf
Firuza Nazarli <nazarlifiruza0@gmail.com>
2025-10-15 10:25:20 +0400

    new

--- src/Second.java
+++ src/Second.java
@@ -4,10 +4,34 @@ public class Second {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("ok");
 
 
+        System.out.print("Neçəlik vurma cədvəli çap edilsin? (məs: 10): ");
+        int n = sc.nextInt();
+
+        System.out.println(n + "x" + n + " Vurma Cədvəli ");
+
+        System.out.print("    ");
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("%4d", i);
+        }
+        System.out.println();
+        System.out.println("------------------------------------------------");
+
+
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("%2d |", i);
+            for (int j = 1; j <= n; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
 
 
     }
 }
+
+
+
+
+
